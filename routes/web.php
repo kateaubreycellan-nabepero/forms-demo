fec7aefd245ca682cbca5bbd19dd9596c9a61968
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-09 11:35:39 +0800

    Basic usage of middleware

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin/user/roles', ['middleware'=>'role', function() {
+
+    return "test";
+
+}]);
