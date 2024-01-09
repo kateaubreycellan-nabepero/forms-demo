965db2a7dcefe708730687998a4ad7a02ddbfac7
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-09 16:20:33 +0800

    Database seeder configuration

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'role_id' => 2,
+            'email' => Str::random(10).'@test.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
