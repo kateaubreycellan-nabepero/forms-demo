fa31b1874e4bc6de9f10bb4d8780168e77d24eea
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-09 13:01:01 +0800

    Flashing sessions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,19 +24,29 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         // Setting a session
-        $request->session()->put(['some text'=>'more text here']);
+        // $request->session()->put(['some text'=>'more text here']);
 
         // Another way of setting a session, but globally
-        session(['more sessions'=>'your session']);
+        // session(['more sessions'=>'your session']);
 
         // Updating a sessions
-        session(['more sessions'=>'replaced session']);
+        // session(['more sessions'=>'replaced session']);
 
         // Deleting sessions
-        $request->session()->forget('more sessions');
+        // $request->session()->forget('more sessions');
+
+        // Flashing data
+        // Does not persist, only stays on the current request and is deleted on next request
+        $request->session()->flash('temporary message', 'some message');
+
+        // Keep data a bit longer
+        $request->session()->reflash();
+
+        // Keeps the session
+        $request->session()->keep('temporary message');
 
         // Delete all sessions
-        $request->session()->flush();
+        // $request->session()->flush();
 
         // Reading sessions
         return $request->session()->all();
