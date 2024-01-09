b72a99d09a6ac72cdcf8a078582801bee5a25572
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-09 11:04:55 +0800

    Retrieving authenticated user data

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (/* $email, $password */) {
+    // return view('welcome');
+
+    // Check if user is logged in
+    if ($auth = Auth::check())
+    {
+        // Logged in user
+        return $auth;
+    }
+    // Not logged in
+    // return 0;
+
+    // Redirect to a specific endpoint if filled up with the correct credentials
+    $email = '';
+    $password = '';
+
+    if (Auth::attempt(['email' => $email, 'password' => $password]))
+    {
+        return redirect()->intended('/home');
+    }
+
+    return 0;
+
+
 });
 
 Auth::routes();
