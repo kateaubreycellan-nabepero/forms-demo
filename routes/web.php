85fad7f91e2988ac1b16ae8667d2eece663275a9
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-09 15:04:12 +0800

    Create route for sending mail

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+
+    $data = [
+        'title'=>"more title",
+        'content'=>'more content',
+    ];
+
+    Mail::send('emails.test', $data, function($message) {
+        $message->to("test@test.com", "Test");
+        $message->subject("Hello!");
+
+    });
+
 });
